89294706816a24a119f0a01f63837a8e5284965f
Максим Петросян <108141375+Maxim8753@users.noreply.github.com>
2022-07-06 13:47:02 +0300

    Update Практическое задание 6.py

--- Практическое задание 6.py
+++ Практическое задание 6.py
@@ -1,6 +1,8 @@
 def int_func(chart):
     text = input('введите текст маленькими латинскими буквами разделенные пробелом: ')
     return text.title()
+
+
 words = input('введите текст маленькими латинскими буквами разделенные пробелом: ').split()
 for word in words:
     print(int_func(word))
